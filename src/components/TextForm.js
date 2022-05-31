94ebc88fa737cac9297927e7b4072e71712614b4
Rahul Sharma <58264599+OrneyVortex@users.noreply.github.com>
2022-05-31 23:58:26 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,35 +8,40 @@ export default function TextForm(props) {
         console.log("Uppercase was clicked");
         let newText=text.toUpperCase();
         setText(newText)
+        props.showAlert("Converted to uppercase","success");
     }
     const handleClearClick = () =>{
         let newText="";
         setText(newText)
+        props.showAlert("All clear","Success");
     }
     const handleLoClick = () =>{
         let newText=text.toLowerCase();
         setText(newText)
+        props.showAlert("converted to lowercase","Success",);
     }
     const handleOnChange=(event) =>{
         console.log("On Change");
         setText(event.target.value)
+
+        
     }
     return (
         <>
         <div className='container' style={{color: props.mode==='dark'?'white':'black'}}>
             <h1 >{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" style={{backgroundColor: props.mode==='dark'?'grey':'white' , color: props.mode==='dark'?'white':'black'}}></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" style={{backgroundColor: props.mode==='dark'?'#16466e':'white' , color: props.mode==='dark'?'white':'black'}}></textarea>
             </div>  
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
-            <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear All</button>
+            <button disabled={text.length===0} className="btn btn-primary my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleClearClick}>Clear All</button>
         </div>
         <div className="container my-4" style={{color: props.mode==='dark'?'white':'black'}}>
             <h1>Your text summary</h1>
             <p>{text.length} characters</p> 
-            <p>{text.split(" ").length} words</p> 
-            <p>{0.008*text.split(" ").length} minutes read</p>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words</p> 
+            <p>{0.008*text.split(" ").filter((element)=>{return element.length!==0}).length} minutes read</p>
             <h3>Preview</h3> 
             <p>{text}</p>
         </div>
